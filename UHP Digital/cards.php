5bfd4d2d79020d3054161973c5d5a319b011c5a1
filiposk <fpavlovic1995@gmail.com>
2018-09-13 17:01:57 +0200

    filiposk
    Spremanje podataka u bazu, i izvlacenje u obliku kartice

--- UHP Digital/cards.php
+++ UHP Digital/cards.php
@@ -8,26 +8,39 @@
 <body>
 <?php include_once "template/navigation.php" ?> 
 
-<div class="flip-container centered" style="width:30%;border:1px solid black;">
-      <div class="flipper">
-         <div class="front"></div>
+<?php
+$query =  $conn->prepare("select * from user ;");
+$query->execute();
+$result = $query->fetchAll(PDO::FETCH_OBJ);
+?>
+ <?php foreach($result as $row): ?>
+ <div style="margin-top: 25px;"></div><br>
+<div style="width:30%; border:1px solid black; margin: auto; background-color:<?php echo $row->Color ?>">
+
+  <div>
+  
+  
+            <div class="front"></div>
+            <div style="text-align:right;top:50%;">
+              <img src="./uploads/<?php echo $row->Picture ?>" alt="slika" width="30%" heigt="30%" >
+            </div>
+          
          <div class="back">
-            <h1>Petrus Rex</h1>
-      <p>Web Design & Development</p>
-         <div class="font-a-icons">
-            <div class="icon-group">
-               <span class="bold">T: </span><span class="contact">512-773-0889</span>
-            <div class="icon-group">
-                <span class="bold">E: </span><a class="contact" href="mailto:hello@petrusrex.com" target="_top">hello@petrusrex.com</a>
-            </div>
-            <div class="icon-group">
-               <a class="contact" href="http://www.petrusrex.com" target="_blank">www.petrusrex.com</a>
-            </div>
-         </div>
-      </div>   
-   
-      </div>
-   </div>
+         
+                
+         <p><?php echo $row->FirstName; ?></p>
+          <p><?php echo $row->LastName; ?></p>
+          <p><?php echo $row->Comp_Pos; ?></p>
+          <p><?php echo $row->Phone; ?></p>
+          <p><?php echo $row->Email; ?></p>
+                    
+          
+                   
+      </div> 
+           
+  </div>
+</div>
+<?php endforeach; ?>
   <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
   
   
--- UHP Digital/createcard.php
+++ UHP Digital/createcard.php
@@ -1,4 +1,19 @@
-<?php include_once "config.php" ?>
+<?php
+include_once "config.php";
+if(isset($_POST["add"])){
+    $query =  $conn->prepare("insert into user(FirstName,LastName,Comp_Pos,Phone,Email,Color,Picture)values
+    (:FirstName,:LastName,:Comp_Pos,:Phone,:Email,:Color,:Picture)");
+    unset($_POST["add"]);    
+$query->execute($_POST);
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["Picture"]["name"]);
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    move_uploaded_file($_FILES["Picture"]["tmp_name"], $target_file);
+header("location: cards.php");
+
+}
+?>
+
 <!doctype html>
 <html>
 <head>   
@@ -7,37 +22,41 @@
 
 <body>
 <?php include_once "template/navigation.php" ?> 
-<form class="callout text-center" style="width:500px;margin-left:auto;margin-right:auto;text-align:center;">
+
+
+
+<form method="post" enctype="multipart/form-data" action="<?php echo $_SERVER["PHP_SELF"];?>" class="callout text-center" style="width:500px;margin-left:auto;margin-right:auto;text-align:center;">
     <h2>Create Card</h2>
     <div class="floated-label-wrapper">
-        <label for="firstName">First name</label>
-        <input type="text" id="firstName" name="firstName" placeholder="Full name">
+        <label for="FirstName">First name</label>
+        <input type="text" id="FirstName" name="FirstName" placeholder="First name">
     </div>
     <div class="floated-label-wrapper">
-        <label for="lastName">Last name</label>
-        <input type="text" id="lastName" name="lastName" placeholder="Last name">
+        <label for="LastName">Last name</label>
+        <input type="text" id="LastName" name="LastName" placeholder="Last name">
     </div>
     <div class="floated-label-wrapper">
-        <label for="c&p">Company & position</label>
-        <input type="text" id="c&p" name="c&p" placeholder="Company & position">
-    </div>
-    <div class="floated-label-wrapper">
-        <label for="Email">Email</label>
-        <input type="Email" id="Email" name="Email" placeholder="Email">
+        <label for="Comp_Pos">Company & position</label>
+        <input type="text" id="Comp_Pos" name="Comp_Pos" placeholder="Company & position">
     </div>
     <div class="floated-label-wrapper">
         <label for="Phone">Phone</label>
         <input type="text" id="Phone" name="Phone" placeholder="Phone">
+    </div><div class="floated-label-wrapper">
+        <label for="Email">Email</label>
+        <input type="Email" id="Email" name="Email" placeholder="Email">
     </div>
-    <div class="floated-label-wrapper">
-        <label for="color">Choose color</label>
-        <input type="color" id="color" name="color" placeholder="Choose color">
+   </div><div class="floated-label-wrapper">
+        <label for="Color">Choose color</label>
+        <input type="color" id="Color" name="Color">
     </div>
-    <div class="floated-label-wrapper">
-        <label for="Picture">Picture</label>
-        <input  style="text-align:center" type="file" id="Picture" name="Picture" accept="image/*" placeholder="Picture">
+    </div><div class="floated-label-wrapper">
+        <label for="Picture">Add picture</label>
+        <input type="file" name="Picture" accept="image/*">
     </div>
-    <input class="button expanded" type="submit" value="Create card">
+   
+    
+    <button type="submit" name="add" class="button">Create card</button>
 </form>
 
   
